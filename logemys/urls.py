ca48750f83cf86fbf55bf266ca18ac99f773b2f4
Mastermind <86709307+Mastermindx33@users.noreply.github.com>
2021-07-16 21:05:59 +0530

    __init__Entries

--- logemys/urls.py
+++ logemys/urls.py
@@ -10,5 +10,11 @@ urlpatterns = [
     # Topics page
     path('topics/', views.topics, name='topics'),
     # One Topic
-    path('topics/<int:topic_id>/', views.topic, name='topic')
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # New Topic
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # New Entry
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # Edit Entry
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
--- logemys/views.py
+++ logemys/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Topic
+from django.shortcuts import render, redirect
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 
 # Create your views here.
@@ -21,3 +22,59 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'logemys/topic.html', context)
+
+
+def new_topic(request):
+    """Add new topic"""
+    if request.method != 'POST':
+        # No data submitted, create a blank form
+        form = TopicForm()
+    else:
+        # POST , Process form
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('logemys:topics')
+
+    # Display a blank or invalid form
+    context = {'form': form}
+    return render(request, 'logemys/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # No data submitted, create a blank form
+        form = EntryForm()
+    else:
+        # POST data submitted, Process Data.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('logemys:topic', topic_id=topic_id)
+    # Display a blank or invalid form
+    context = {'topic': topic, 'form': form}
+    return render(request, 'logemys/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Editing an existing entry"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # Initial request; Pre fill the form with the current entry
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; Process data.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('logemys:topic', topic_id=topic.id)
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'logemys/edit_entry.html', context)
