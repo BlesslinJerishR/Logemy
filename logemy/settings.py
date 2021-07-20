17f3bf5c4e60de2d3735fc8c79f8ca011ae92fd1
Mastermind <86709307+Mastermindx33@users.noreply.github.com>
2021-07-20 10:52:39 +0530

    __bugs__settings

--- logemy/settings.py
+++ logemy/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'logemy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
