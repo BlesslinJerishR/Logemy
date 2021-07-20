29e59a4a75fe30b68ac9dcf0b0d60a4a27f1a5b7
Mastermind <86709307+Mastermindx33@users.noreply.github.com>
2021-07-20 10:39:13 +0530

    __bugs__static

--- logemy/settings.py
+++ logemy/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "../logemys/static/")
 STATIC_URL = '/static/'
 
 # Default primary key field type
