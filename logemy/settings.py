2b664fcfd0d35c9e58c70efee4671cbdf889d929
Mastermind <86709307+Mastermindx33@users.noreply.github.com>
2021-07-20 10:45:35 +0530

    __bugs__Settings

--- logemy/settings.py
+++ logemy/settings.py
@@ -52,8 +52,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'logemy.urls'
 
 TEMPLATES = [
