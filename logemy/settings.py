3672d12847afa91f9a5db96c6d2cdf922bc8ae8c
Mastermind <86709307+Mastermindx33@users.noreply.github.com>
2021-07-20 11:03:51 +0530

    __bugs__settings

--- logemy/settings.py
+++ logemy/settings.py
@@ -25,7 +25,7 @@ Debug = False
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['logemy.herokuapp.com', 'https://logemy.herokuapp.com',]
 
 
 # Application definition
