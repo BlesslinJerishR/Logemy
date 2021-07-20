f9fc5b7768e8e8eb8cc01d6468fc45bddc2972e6
Mastermind <86709307+Mastermindx33@users.noreply.github.com>
2021-07-20 11:37:58 +0530

    Update settings.py

--- logemy/settings.py
+++ logemy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3abv-0)ek_pdt6z*!@t(lwidl+0^*var)0%93y+n)7x6h(5q=-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*,]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
