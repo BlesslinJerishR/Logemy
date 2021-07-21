0bd5dfdb8690c42a8ccae8a115319df7eae96949
Mastermind <86709307+Mastermindx33@users.noreply.github.com>
2021-07-21 11:24:07 +0530

    Update urls.py

--- logemys/urls.py
+++ logemys/urls.py
@@ -20,7 +20,5 @@ urlpatterns = [
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
     # Edit Entry
     path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
-    # # Static css
-    # path('', views.css)
 ]
 urlpatterns += staticfiles_urlpatterns()
