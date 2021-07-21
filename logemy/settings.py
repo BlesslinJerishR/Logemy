9c3658f16e77c0a540e6fe407039ec54d5cd7c61
Mastermind <86709307+Mastermindx33@users.noreply.github.com>
2021-07-21 08:39:04 +0530

    __bugs__req

--- logemy/settings.py
+++ logemy/settings.py
@@ -133,10 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # My Settings
 LOGIN_URL = 'users:login'
 
-# Heroku settings.
-import django_heroku
-django_heroku.settings(locals())
-
 if os.environ.get('DEBUG') == 'TRUE':
     DEBUG = True
 elif os.environ.get('DEBUG') == 'FALSE':
